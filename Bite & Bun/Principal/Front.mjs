aa59ce2a3281c211044f2d12110cf19009282037
Bernardo.pSenai <bernardo_gasparini@estudante.sesisenai.org.br>
2026-07-16 19:54:23 -0300

    concerto do front

--- Bite & Bun/Principal/Front.mjs
+++ Bite & Bun/Principal/Front.mjs
@@ -1,6 +1,6 @@
 import { faturamentoTotal } from "./faturamento.mjs";
 import { faturamento, maiorEmenor, ultrapassamValorDefinido } from "./faturamento.mjs";
 console.log(`Faturamento Total: ${faturamento()}
-Pedidos com o valor ultrapassado do predifinido: ${ultrapassamValorDefinido()}
-Maior e Menor valor de venda: `);maiorEmenor()
-console.log("Ticket médio: "+faturamentoTotal().toFixed(2))
+Pedidos com o valor ultrapassado do pré definido: ${ultrapassamValorDefinido()}`);
+maiorEmenor();
+console.log("Ticket médio: "+faturamentoTotal().toFixed(2));
