f98a429270cccb19e4e251dc52090d3e2a40442b
Bernardo.pSenai <bernardo_gasparini@estudante.sesisenai.org.br>
2026-08-26 20:13:10 -0300

    Delete Bite & Bun/Principal directory
    .

--- Bite & Bun/Principal/Front.mjs
+++
@@ -1,6 +0,0 @@
-import { faturamentoTotal } from "./faturamento.mjs";
-import { faturamento, maiorEmenor, ultrapassamValorDefinido } from "./faturamento.mjs";
-console.log(`Faturamento Total: ${faturamento()}
-Pedidos com o valor ultrapassado do pré definido: ${ultrapassamValorDefinido()}`);
-maiorEmenor();
-console.log("Ticket médio: "+faturamentoTotal().toFixed(2));
